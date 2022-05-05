8757dd174d283dddf69883e14a97bf5700d3e5b3
Your Name <you@example.com>
2022-05-05 15:42:25 +0900

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,14 @@
+print('Hello, Django girls!')
+
+if 3 > 2:
+    print('It works!')
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi()
